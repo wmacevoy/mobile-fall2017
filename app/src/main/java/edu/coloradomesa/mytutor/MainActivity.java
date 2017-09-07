28ee664c64da0a174768a195929c090f954ce1ec
Warren MacEvoy <wmacevoy@gmail.com>
2017-09-07 13:17:16 -0600

    Initial commit

---
+++ app/src/main/java/edu/coloradomesa/mytutor/MainActivity.java
@@ -0,0 +1,52 @@
+package edu.coloradomesa.mytutor;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuView;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private MenuView.ItemView mScheduleItemView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Log.i("MyTutor","bottom nav item " + item.getItemId());
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_schedule:
+                    mTextMessage.setText(R.string.title_schedule);
+                    Log.i("MyTutor","schedule tab");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        mScheduleItemView = (MenuView.ItemView) findViewById(R.id.navigation_schedule);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
